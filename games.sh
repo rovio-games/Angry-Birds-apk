65eaaf80f90a059b94c8517b3a6978e20905007f
mobiledesres <45989029+mobiledesres@users.noreply.github.com>
2021-11-29 12:19:46 -0800

    games.sh "Angry Birds Classic"

--- games.sh
+++ games.sh
@@ -1,6 +1,6 @@
 declare -a GAMES
 
-GAMES[${#GAMES[@]}]="Angry Birds"
+GAMES[${#GAMES[@]}]="Angry Birds Classic"
 GAMES[${#GAMES[@]}]="Angry Birds Seasons"
 GAMES[${#GAMES[@]}]="Angry Birds Rio"
 GAMES[${#GAMES[@]}]="Angry Birds Space"

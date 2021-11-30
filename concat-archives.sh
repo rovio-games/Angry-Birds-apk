7a7959b3803816a485b603c75507a4b9635886ef
mobiledesres <45989029+mobiledesres@users.noreply.github.com>
2021-11-30 15:07:22 -0800

    concat-archives.sh: use command line arguments for source and target directories

--- concat-archives.sh
+++ concat-archives.sh
@@ -1,6 +1,17 @@
+if [[ $# -lt 2 ]]
+then
+    echo "Usage: \"$BASH_SOURCE\" <source_directory> <target_directory>" >&2
+    echo "* <source_directory>: The source directory containing only sub-directories, each containing split archives for one single game" >&2
+    echo "* <target_directory>: The target directory for all .apk or .xapk files" >&2
+
+    exit 1
+else
+    srcDir="$1"
+    targetDir="$2"
+fi
+
 DIRNAME="$(dirname "$(realpath "$BASH_SOURCE")")"
 
-source "$DIRNAME/dirs.sh"
 source "$DIRNAME/games.sh"
 
 # Concatenate split archives into a .apk or .xapk file, using the "cat" program
@@ -21,7 +32,7 @@ function concat_archive {
 }
 
 # concatenate all split archives
-# Usage: concat_all "<source_directory>" "<target_directory>"
+# Usage: concat_all "<split_archives_directory>" "<target_directory>"
 function concat_all {
     local srcDir="$1" targetDir="$2"
 
@@ -29,8 +40,8 @@ function concat_all {
 
     for game in "${GAMES[@]}"
     do
-        local srcDir="$DIRNAME/$SPLIT_DIR/$game"
-        concat_archive "$srcDir" "$targetDir" &
+        local splitDir="$srcDir/$game"
+        concat_archive "$splitDir" "$targetDir" &
     done
 
     for game in "${GAMES[@]}"
@@ -38,3 +49,5 @@ function concat_all {
         wait -fn
     done
 }
+
+concat_all "$srcDir" "$targetDir"
